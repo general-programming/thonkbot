9d374d914790e081d61d278e71e8f86c4b7e8055
Henry <omgredstone@gmail.com>
2017-10-16 18:21:47 +0100

    Prevented error on local copies by checking for production env

--- thonk/twitter.py
+++ thonk/twitter.py
@@ -1,11 +1,13 @@
 from peony import PeonyClient
 from os import getenv
+from . import utils
 import json
 
-with open(getenv("TWITTER_CONFIG"), mode='r') as file:
-    twitter_opts = json.load(file)
+if utils.is_deployed():
+    with open(getenv("TWITTER_CONFIG"), mode='r') as file:
+        twitter_opts = json.load(file)
 
-twitter_bot = PeonyClient(**twitter_opts)
+    twitter_bot = PeonyClient(**twitter_opts)
 
 async def tweet(message: str):
     return await twitter_bot.api.statuses.update.post(status=message)
