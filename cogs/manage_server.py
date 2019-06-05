a5aea948e4b15dea2e47c7abf669e50ca9000979
Henry <omgredstone@gmail.com>
2019-06-05 22:23:01 +0100

    Fixed broken argument typehint (thanks dpy)

--- cogs/manage_server.py
+++ cogs/manage_server.py
@@ -1,12 +1,16 @@
 from discord import VoiceRegion, Embed, Color
 from discord.ext import commands
-from discord.ext.commands import Bot, Cog, Context
+from discord.ext.commands import Bot, Cog, Context, Converter
 from thonk.utils import require_tag
+
+class VoiceRegionConverter(Converter):
+    async def convert(self, ctx: Context, argument: str):
+        return VoiceRegion(argument)
 
 class ServerManagementCog(Cog):
     @commands.command()
     @require_tag("member")
-    async def switch_region(self, ctx: Context, new_region: VoiceRegion):
+    async def switch_region(self, ctx: Context, new_region: VoiceRegionConverter):
         """Change the server voice region."""
 
         await ctx.guild.edit(reason=f"Switch region command triggered by {ctx.author}", region=new_region)
